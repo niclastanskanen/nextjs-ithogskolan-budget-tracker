afa04b748b181cabec051f8c9e1ce8a83c52cc9f
Niclas <niclastanskanen@hotmail.com>
2024-10-23 09:54:13 +0200

    Refactor Header component to include a styled header with a title and mode toggle

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,14 @@ import { ModeToggle } from "./ModeToggle";
 
 const Header = () => {
   return (
-    <div>
-      Header
-      <ModeToggle />
-    </div>
+    <header className="bg-background border-b">
+      <div className="container flex flex-row justify-between items-center mx-auto px-4 py-3 space-y-2">
+        <h1 className="text-xl sm:text-2xl font-bold text-primary">
+          Budget Tracker
+        </h1>
+        <ModeToggle />
+      </div>
+    </header>
   );
 };
 
